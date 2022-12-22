ede522aae73e9c48f43722e29c92e08951330fa8
root <root@LAPTOP-4IMM4QUP.localdomain>
2022-12-22 17:28:04 +0900

    setting.php作成

---
+++ setting.php
@@ -0,0 +1,11 @@
+<?php
+
+date_default_timezone_set("Asia/Tokyo");
+
+define( 'DB_HOST', 'localhost' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASS', 'chinzanso1215' );
+define( 'DB_NAME', 'bb-database' );
+define( 'DB_DSN', 'mysql:host=localhost;dbname=bb-database' );
+
+?>
